ba9fa5a424f80d5835163ad663dcc31da9800f21
abhiramiaj <75014747+abhiramiaj@users.noreply.github.com>
2021-11-26 16:55:51 +0530

    Update EregkimBzer.m

--- EregkimBzer.m
+++ EregkimBzer.m
@@ -1,22 +1,14 @@
+%function to findout stiffness of regular staggered composite after VIF
 function s1 = EregkimBzer(Ep,Em,num,b,Lb,h,ar)
-%%After vif B=0
-%vertical matrix thickness is assumed to be same as that of horizontal
-%%matrix thickness so that Lb=h/2
-%%Default value of E taken from Kim eqn 6
-%%2b is the width of the platelet
 Lp=ar*b*2;
-% Lb=h/2;
 La=(Lp/2)-Lb;
 Gm=Em/(2*(1+num));
 phi=(2*b)/((2*b)+h);
-
 rho=La/b;
 al=Gm/Ep;
 bet=Em/Ep;
 zet=Lb./La;
-
 A=0.5*rho*sqrt(al)*sqrt(phi/(1-phi));
-%B=bet/(phi*zet);
 B=0;
 sinhA=sinh(A);
 coshA=cosh(A);
@@ -25,7 +17,6 @@ D=phi+((1-phi)*bet)+(2*zet*phi);
 E=(1-bet+(2*bet/phi)+(4*zet))*A*tanh(A);
 F=1+bet+B;
 Ecom_eqn6=Ep*(C*D)/(E+F);
-%Ecomp_eqn7=Ep*phi*((A*sinh(A))+(B*cosh(A)))/(cosh(A)+(A*sinh(A))+(B*cosh(A)));
 s1=Ecom_eqn6;
 double(s1);
 end
