fa5758e7227649626b764a0bab8c8418e96ab1a2
abhiramiaj <75014747+abhiramiaj@users.noreply.github.com>
2021-11-29 11:03:27 +0530

    Update swstrengthsmfBzer.m

--- swstrengthsmfBzer.m
+++ swstrengthsmfBzer.m
@@ -1,27 +1,9 @@
 %%Second FAILURE MODE IN A SW STAGGERED COMPOSITE
-%%to find ratio of strength before and after failure
-%%vifand hif first occuring cases are considered in this code
-% after vif B=0 in kims model
-%after hif, only vif is expected and strength is calculated by assuming the
-%composite as a continuous fiber type without the horizontal interface
-% of a SW stagg comp under vif and  hif criteria using Kim s
-%%exact model
 %%%For comparison, the updated values (after the fmf)of ratios are used.
 function swstrengthsmfBzer =swstrengthsmfBzer(n,Ep,Em,num,b,Lb,h,rho,sigpcrit,sigmcrit,taumcrit)
-%E = Eswkim(Ep,Em,num,b,h,rho,n)
-%Eaftrvif = EswkimBzer(Ep,Em,num,b,h,rho,n) %E after vif
 Lp=rho*2*b;
-%Lb=h/2%Assuming both vertical and horizontal matrix thicknesses are the same
 L=Lp+(2*Lb);
-%L1=Lp+(2*Lb)
-% x1=Lp
-% x2=(4*L1/n)-Lb
-% x3=(3*L1/n)-Lb
-% x4=(2*L1/n)-Lb
-% x5=(L1/n)-Lb
 La1=(L./n)-(2*Lb);
-% La1=(Lp/n)-(2*Lb) %changed on 2020aug12 See fig in zhang 2010 for explanation
-%each platelet is shifted by Length of platelet/n including Lb
 x11=Lp;
 x22=La1+(2*Lb);
 
