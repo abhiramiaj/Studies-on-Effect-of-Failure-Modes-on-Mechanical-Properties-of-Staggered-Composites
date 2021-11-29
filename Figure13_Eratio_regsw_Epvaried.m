464901c78f4661586ed58f3111b3659cc2d54fb4
abhiramiaj <75014747+abhiramiaj@users.noreply.github.com>
2021-11-29 10:10:53 +0530

    Update Figure13_Eratio_regsw_Epvaried.m

--- Figure13_Eratio_regsw_Epvaried.m
+++ Figure13_Eratio_regsw_Epvaried.m
@@ -5,13 +5,10 @@ num1=0.38;
 sigmcrit1=43.4;
 sigpcrit1=2500;
 taumcrit1=25;
-%taumcrit is 0.576 times sigmamcrit
 subplot(1,2,1);
 b=10;
-%h=5;
 hnol=1/50;
 vf=0.8;
-%Lb=2.5;
 rr=4:1:120;
 [SMP1,SMP2,SMP3,SMP4,SMP5,SMP6,Lb,h]=deal(zeros(size(rr)));
 for i=1:numel(rr)
@@ -46,7 +43,6 @@ xlabel ('\rho','fontweight','bold', 'fontsize', 15)
 ylabel ('E/E_{B=0}, Regularly Staggered','fontweight','bold', 'fontsize', 15)
 
 set(gcf,'color','w')
-%of regularly staggered composite using Kim6 
 axis square
 legend('E_p/E_m=10','E_p/E_m=50','E_p/E_m=100','E_p/E_m=200','E_p/E_m=500','E_p/E_m=1000','fontsize',12)
 subplot(1,2,2);
