06c6bb3fe5c94674021b6171f6815d47842606b8
abhiramiaj <75014747+abhiramiaj@users.noreply.github.com>
2022-01-17 12:10:38 +0530

    Update Figure23_Eratio_regsw_5materialsets.m

--- Figure23_Eratio_regsw_5materialsets.m
+++ Figure23_Eratio_regsw_5materialsets.m
@@ -1,5 +1,14 @@
+clc; clear all;
 figure
 
+%Ep: Young's modulus of platelet
+%Em: Young's modulus of matrix
+%num1: Poisson's ratio of matrix
+%sigmcrit: Normal strength of matrix
+%sigpcrit: Normal strenght of platelet
+%taumcrit: Shear strength of matrix
+
+%%%%%%%%%%MP1%%%%%%%%%%
 Ep1=1200;
 Em1=0.67; 
 num1=0.4;
@@ -7,6 +16,7 @@ sigmcrit1=1;
 sigpcrit1=40;
 taumcrit1=0.576;
 
+%%%%%%%%%%MP2%%%%%%%%%%
 Ep2=1e5;
 Em2=200;   
 num2=0.4;
@@ -14,15 +24,15 @@ sigmcrit2=20;
 sigpcrit2=200;
 taumcrit2=11.52;
 
+%%%%%%%%%%MP3%%%%%%%%%%
 Ep3=2.2e5;
 Em3=1100;
-%num3=0.38;
 num3=0.4;
 sigmcrit3=30;
 sigpcrit3=3250;
 taumcrit3=17.3;
 
-
+%%%%%%%%%%MP4%%%%%%%%%%
 Ep4=2.2e5;
 Em4=3000;
 %num4=0.38;
@@ -31,7 +41,7 @@ sigmcrit4=40;
 sigpcrit4=3250;
 taumcrit4=23.1;
 
-
+%%%%%%%%%%MP5%%%%%%%%%%
 Ep5=1e5;
 Em5=4000;  
 num5=0.4;
@@ -40,21 +50,21 @@ sigpcrit5=200;
 taumcrit5=11.52;
 
 subplot(1,2,1);
-rr=4:1:120;
+rr=4:1:120; %platelet aspect ratio
 [SMP1,SMP2,SMP3,SMP4,SMP5]=deal(zeros(size(rr)));
-hnol=1/50;
-vf=0.8;
+hnol=1/50;%half non-overlap length to overlap length ratio
+vf=0.8;%volume fraction
 b=10; %%2b is the width of platelet
 
 
 for i=1:numel(rr)
-Lb(i)=hnol*rr(i)*b/(1+hnol);
-h(i)=(2*b*rr(i)*2*b./(vf*(0.5*2*b*rr(i) + Lb(i)))) - (2*b);
-SMP1(i)=(Eregkim(Ep1,Em1,num1,b,Lb(i),h(i),rr(i)))./EregkimBzer(Ep1,Em1,num1,b,Lb(i),h(i),rr(i));
-SMP2(i)=(Eregkim(Ep2,Em2,num2,b,Lb(i),h(i),rr(i)))./EregkimBzer(Ep2,Em2,num2,b,Lb(i),h(i),rr(i));
-SMP3(i)=(Eregkim(Ep3,Em3,num3,b,Lb(i),h(i),rr(i)))./EregkimBzer(Ep3,Em3,num3,b,Lb(i),h(i),rr(i));
-SMP4(i)=(Eregkim(Ep4,Em4,num4,b,Lb(i),h(i),rr(i)))./EregkimBzer(Ep4,Em4,num4,b,Lb(i),h(i),rr(i));
-SMP5(i)=(Eregkim(Ep5,Em5,num5,b,Lb(i),h(i),rr(i)))./EregkimBzer(Ep5,Em5,num5,b,Lb(i),h(i),rr(i));
+    Lb(i)=hnol*rr(i)*b/(1+hnol);
+    h(i)=((2*b*b*rr(i))./(vf*(b*rr(i) + Lb(i)))) - (2*b);
+    vf1(i)=(2*b*b*rr(i))./((2*b + h(i))*(rr(i)*b + Lb(i)));SMP1(i)=(Eregkim(Ep1,Em1,num1,b,Lb(i),h(i),rr(i)))./EregkimBzer(Ep1,Em1,num1,b,Lb(i),h(i),rr(i));
+    SMP2(i)=(Eregkim(Ep2,Em2,num2,b,Lb(i),h(i),rr(i)))./EregkimBzer(Ep2,Em2,num2,b,Lb(i),h(i),rr(i));
+    SMP3(i)=(Eregkim(Ep3,Em3,num3,b,Lb(i),h(i),rr(i)))./EregkimBzer(Ep3,Em3,num3,b,Lb(i),h(i),rr(i));
+    SMP4(i)=(Eregkim(Ep4,Em4,num4,b,Lb(i),h(i),rr(i)))./EregkimBzer(Ep4,Em4,num4,b,Lb(i),h(i),rr(i));
+    SMP5(i)=(Eregkim(Ep5,Em5,num5,b,Lb(i),h(i),rr(i)))./EregkimBzer(Ep5,Em5,num5,b,Lb(i),h(i),rr(i));
 end
 plot(rr,SMP1,'linewidth', 1.5)
 hold on;
@@ -72,7 +82,7 @@ xlabel ('\rho','fontweight','bold', 'fontsize', 15)
 ylabel ('E/E_{B=0}, Regularly Staggered','fontweight','bold', 'fontsize', 15)
 axis square
 set(gcf,'color','w')
-%of regularly staggered composite using Kim6 
+
 legend('MP1','MP2','MP3','MP4','MP5','fontsize',12)
 subplot(1,2,2);
 
@@ -84,13 +94,13 @@ b=10; %%2b is the width of platelet
 Lb=2.5;
 %%MP1
 for i=1:numel(rr)
-Lb(i)=hnol*rr(i)*b/(1+hnol);
-h(i)=(2*b*rr(i)*2*b./(vf*(0.5*2*b*rr(i) + Lb(i)))) - (2*b);
-SMP1(i)=(Eswkim(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i)))./EswkimBzer(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i));
-SMP2(i)=(Eswkim(n,Ep2,Em2,num2,b,Lb(i),h(i),rr(i)))./EswkimBzer(n,Ep2,Em2,num2,b,Lb(i),h(i),rr(i));
-SMP3(i)=(Eswkim(n,Ep3,Em3,num3,b,Lb(i),h(i),rr(i)))./EswkimBzer(n,Ep3,Em3,num3,b,Lb(i),h(i),rr(i));
-SMP4(i)=(Eswkim(n,Ep4,Em4,num4,b,Lb(i),h(i),rr(i)))./EswkimBzer(n,Ep4,Em4,num4,b,Lb(i),h(i),rr(i));
-SMP5(i)=(Eswkim(n,Ep5,Em5,num5,b,Lb(i),h(i),rr(i)))./EswkimBzer(n,Ep5,Em5,num5,b,Lb(i),h(i),rr(i));
+    Lb(i)=hnol*rr(i)*b/(1+hnol);
+    h(i)=((2*b*b*rr(i))./(vf*(b*rr(i) + Lb(i)))) - (2*b);
+    vf2(i)=(2*b*b*rr(i))./((2*b + h(i))*(rr(i)*b + Lb(i)));SMP1(i)=(Eswkim(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i)))./EswkimBzer(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i));
+    SMP2(i)=(Eswkim(n,Ep2,Em2,num2,b,Lb(i),h(i),rr(i)))./EswkimBzer(n,Ep2,Em2,num2,b,Lb(i),h(i),rr(i));
+    SMP3(i)=(Eswkim(n,Ep3,Em3,num3,b,Lb(i),h(i),rr(i)))./EswkimBzer(n,Ep3,Em3,num3,b,Lb(i),h(i),rr(i));
+    SMP4(i)=(Eswkim(n,Ep4,Em4,num4,b,Lb(i),h(i),rr(i)))./EswkimBzer(n,Ep4,Em4,num4,b,Lb(i),h(i),rr(i));
+    SMP5(i)=(Eswkim(n,Ep5,Em5,num5,b,Lb(i),h(i),rr(i)))./EswkimBzer(n,Ep5,Em5,num5,b,Lb(i),h(i),rr(i));
 end
 plot(rr,SMP1,'linewidth', 1.5)
 hold on;
@@ -110,6 +120,5 @@ xlabel ('\rho','fontweight','bold', 'fontsize', 15)
 ylabel ('E/E_{B=0}, Stairwise Staggered','fontweight','bold', 'fontsize', 15)
 axis square
 set(gcf,'color','w')
-%of regularly staggered composite using Kim6 
 legend('MP1','MP2','MP3','MP4','MP5','fontsize',12)
 set(gca,'fontsize',15)
