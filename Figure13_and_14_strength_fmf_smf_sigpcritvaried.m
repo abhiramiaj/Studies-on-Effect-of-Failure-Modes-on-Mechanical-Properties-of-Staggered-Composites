416489abb29cd333dd31f405dbea22431fd71d7b
abhiramiaj <75014747+abhiramiaj@users.noreply.github.com>
2022-10-18 13:08:36 +0530

    Update Figure13_and_14_strength_fmf_smf_sigpcritvaried.m

--- Figure13_and_14_strength_fmf_smf_sigpcritvaried.m
+++ Figure13_and_14_strength_fmf_smf_sigpcritvaried.m
@@ -91,7 +91,7 @@ ylim([0 10])
 %'$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=200$',...
 %'Zhang et al., 2010, $\frac{\sigma^p_{critical}}{\tau^m_{critical}}=10$','fontsize',9);
 lg1=legend('$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=10$',...
-    '$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=25, 50, 100, 150, 200$',...
+    '$\frac{\sigma^p_{critical}}{\tau^m_{critical}}\geq25$',...
     'Zhang et al., 2010, $\frac{\sigma^p_{critical}}{\tau^m_{critical}}=10$','fontsize',9);
 lg1.FontSize=12;
 set(lg1,'Interpreter','latex')
@@ -169,7 +169,7 @@ axis square
 %'$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=200$', ...
 %'Zhang et al., 2010, $\frac{\sigma^p_{critical}}{\tau^m_{critical}}=10$','fontsize',9);
 lg2=legend('$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=10$',...
-    '$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=25, 50, 100, 150, 200$',...
+    '$\frac{\sigma^p_{critical}}{\tau^m_{critical}}\geq25$',...
     'Zhang et al., 2010, $\frac{\sigma^p_{critical}}{\tau^m_{critical}}=10$','fontsize',9);
 
 lg2.FontSize=12;
@@ -217,7 +217,7 @@ axis square
 
 %lg3=legend('$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=10$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=25$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=50$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=100$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=150$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=200$','fontsize',9);
 lg3=legend('$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=10$',...
-    '$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=25, 50, 100, 150, 200$','fontsize',9);
+    '$\frac{\sigma^p_{critical}}{\tau^m_{critical}}\geq25$','fontsize',9);
 
 lg3.FontSize=12;
 set(lg3 ,'Interpreter','latex')
@@ -255,160 +255,160 @@ set(gca,'fontsize',15)
 axis square
 %lg4=legend('$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=10$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=25$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=50$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=100$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=150$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=200$','fontsize',9);
 lg4=legend('$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=10$',...
-        '$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=25, 50, 100, 150, 200$','fontsize',9);
+        '$\frac{\sigma^p_{critical}}{\tau^m_{critical}}\geq25$','fontsize',9);
 lg4.FontSize=12;
 set(lg4,'Interpreter','latex')
 
 
-%%%%%%%%%%%UNCOMMENT TO PLOT STRENGTH RATI(FMF/SMF)%%%%%%%%%%%%%%
-% clc; clear all;
-% figure
-% Ep1=220e3;
-% Em1=1.1e3;
-% %Ep/Em set as 200
-% num1=0.38;
-% sigmcrit1=30;
-% %sigpcrit1=200:200:1400;
-% taumcrit1=17.3;
-% sigpcrit1=[10*taumcrit1 25*taumcrit1 50*taumcrit1 100*taumcrit1 150*taumcrit1 200*taumcrit1];   
-% %taumcrit is 0.576 times sigmamcrit
-% subplot(1,2,1);
-% b=10;
-% hnol=1/50;
-% vf=0.8;
-% n=5;
-% rr=1:1:250;
-% %[Lb,h,SMP1,SMP2,SMP3,SMP4,SMP5,SMP6,SMP7,SMP8]=deal(zeros(size(rr)));
-% for i=1:numel(rr)
-% Lb(i)=hnol*rr(i)*b/(1+hnol);
-% h(i)=((2*b*b*rr(i))./(vf*(b*rr(i) + Lb(i)))) - (2*b);
-% vf1(i)=(2*b*b*rr(i))./((2*b + h(i))*(rr(i)*b + Lb(i)));
-% SMP1s(i)=regstrengthsmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(1),sigmcrit1,taumcrit1);
-% SMP2s(i)=regstrengthsmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(2),sigmcrit1,taumcrit1);
-% SMP3s(i)=regstrengthsmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(3),sigmcrit1,taumcrit1);
-% SMP4s(i)=regstrengthsmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(4),sigmcrit1,taumcrit1);
-% SMP5s(i)=regstrengthsmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(5),sigmcrit1,taumcrit1);
-% SMP6s(i)=regstrengthsmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(6),sigmcrit1,taumcrit1);
-% SMP1f(i)=regstrengthfmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(1),sigmcrit1,taumcrit1);
-% SMP2f(i)=regstrengthfmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(2),sigmcrit1,taumcrit1);
-% SMP3f(i)=regstrengthfmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(3),sigmcrit1,taumcrit1);
-% SMP4f(i)=regstrengthfmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(4),sigmcrit1,taumcrit1);
-% SMP5f(i)=regstrengthfmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(5),sigmcrit1,taumcrit1);
-% SMP6f(i)=regstrengthfmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(6),sigmcrit1,taumcrit1);
-% if SMP1s(i)~=SMP1f(i)
-%     rr1(i)=rr(i);
-%     SMP1(i)=SMP1f(i)/SMP1s(i);
-% end
-% if SMP2s(i)~=SMP2f(i)
-%     rr2(i)=rr(i);
-%     SMP2(i)=SMP2f(i)/SMP2s(i);
-% end
-% if SMP3s(i)~=SMP3f(i)
-%     rr3(i)=rr(i);
-%     SMP3(i)=SMP3f(i)/SMP3s(i);
-% end
-% if SMP4s(i)~=SMP4f(i)
-%     rr4(i)=rr(i);
-%     SMP4(i)=SMP4f(i)/SMP4s(i);
-% end
-% if SMP5s(i)~=SMP5f(i)
-%     rr5(i)=rr(i);
-%     SMP5(i)=SMP5f(i)/SMP5s(i);
-% end
-% if SMP6s(i)~=SMP6f(i)
-%     rr6(i)=rr(i);
-%     SMP6(i)=SMP6f(i)/SMP6s(i);
-% end
-% end
-% plot(rr1,SMP1,'linewidth',1.5)
-% hold on;
-% plot(rr2,SMP2,'linewidth',1.5)
-% hold on;
-% plot(rr3,SMP3,'linewidth',1.5)
-% hold on;
-% plot(rr4,SMP4,'linewidth',1.5)
-% hold on;
-% plot(rr5,SMP5,'linewidth',1.5)
-% hold on;
-% plot(rr6,SMP6,'linewidth',1.5)
-% hold on;
-% 
-% set(gca,'fontsize',15)
-% 
-% xlabel ('\rho','fontweight','bold', 'fontsize', 15)
-% ylabel ('Normalized strength ratio, reg (FMF/SMF)','fontweight','bold', 'fontsize', 15)
-% set(gcf,'color','w')
-% 
-% axis square
-% ylim([0 10])
-% lg1=legend('$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=10$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=25$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=50$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=100$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=150$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=200$','fontsize',9);
-% lg1.FontSize=12;
-% set(lg1,'Interpreter','latex')
-% 
-% subplot(1,2,2);
-% 
-% for i=1:numel(rr)
-% Lb(i)=hnol*rr(i)*b/(1+hnol);
-% h(i)=((2*b*b*rr(i))./(vf*(b*rr(i) + Lb(i)))) - (2*b);
-% vf1(i)=(2*b*b*rr(i))./((2*b + h(i))*(rr(i)*b + Lb(i)));
-% SMP1s(i)=swstrengthsmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(1),sigmcrit1,taumcrit1);
-% SMP2s(i)=swstrengthsmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(2),sigmcrit1,taumcrit1);
-% SMP3s(i)=swstrengthsmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(3),sigmcrit1,taumcrit1);
-% SMP4s(i)=swstrengthsmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(4),sigmcrit1,taumcrit1);
-% SMP5s(i)=swstrengthsmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(5),sigmcrit1,taumcrit1);
-% SMP6s(i)=swstrengthsmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(6),sigmcrit1,taumcrit1);
-% SMP1f(i)=swstrengthfmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(1),sigmcrit1,taumcrit1);
-% SMP2f(i)=swstrengthfmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(2),sigmcrit1,taumcrit1);
-% SMP3f(i)=swstrengthfmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(3),sigmcrit1,taumcrit1);
-% SMP4f(i)=swstrengthfmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(4),sigmcrit1,taumcrit1);
-% SMP5f(i)=swstrengthfmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(5),sigmcrit1,taumcrit1);
-% SMP6f(i)=swstrengthfmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(6),sigmcrit1,taumcrit1);
-% if SMP1s(i)~=SMP1f(i)
-%     rr11(i)=rr(i);
-%     SMP11(i)=SMP1f(i)/SMP1s(i);
-% end
-% if SMP2s(i)~=SMP2f(i)
-%     rr22(i)=rr(i);
-%     SMP22(i)=SMP2f(i)/SMP2s(i);
-% end
-% if SMP3s(i)~=SMP3f(i)
-%     rr33(i)=rr(i);
-%     SMP33(i)=SMP3f(i)/SMP3s(i);
-% end
-% if SMP4s(i)~=SMP4f(i)
-%     rr44(i)=rr(i);
-%     SMP44(i)=SMP4f(i)/SMP4s(i);
-% end
-% if SMP5s(i)~=SMP5f(i)
-%     rr55(i)=rr(i);
-%     SMP55(i)=SMP5f(i)/SMP5s(i);
-% end
-% if SMP6s(i)~=SMP6f(i)
-%     rr66(i)=rr(i);
-%     SMP66(i)=SMP6f(i)/SMP6s(i);
-% end
-% end
-% plot(rr11,SMP11,'linewidth',1.5)
-% hold on;
-% plot(rr22,SMP22,'linewidth',1.5)
-% hold on;
-% plot(rr33,SMP33,'linewidth',1.5)
-% hold on;
-% plot(rr44,SMP44,'linewidth',1.5)
-% hold on;
-% plot(rr55,SMP55,'linewidth',1.5)
-% hold on;
-% plot(rr66,SMP66,'linewidth',1.5)
-% hold on;
-% 
-% 
-% xlabel ('\rho','fontweight','bold', 'fontsize', 15)
-% ylabel ('Normalized strength ratio, sw (FMF/SMF)','fontweight','bold', 'fontsize', 15)
-% set(gcf,'color','w')
-% set(gca,'fontsize',15)
-% ylim([0 20])
-% axis square
-% lg2=legend('$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=10$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=25$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=50$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=100$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=150$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=200$','fontsize',9);
-% lg2.FontSize=12;
-% set(lg2 ,'Interpreter','latex')
+%%%%%%%%%%UNCOMMENT TO PLOT STRENGTH RATI(FMF/SMF)%%%%%%%%%%%%%%
+clc; clear all;
+figure
+Ep1=220e3;
+Em1=1.1e3;
+%Ep/Em set as 200
+num1=0.38;
+sigmcrit1=30;
+%sigpcrit1=200:200:1400;
+taumcrit1=17.3;
+sigpcrit1=[10*taumcrit1 25*taumcrit1 50*taumcrit1 100*taumcrit1 150*taumcrit1 200*taumcrit1];   
+%taumcrit is 0.576 times sigmamcrit
+subplot(1,2,1);
+b=10;
+hnol=1/50;
+vf=0.8;
+n=5;
+rr=1:1:250;
+%[Lb,h,SMP1,SMP2,SMP3,SMP4,SMP5,SMP6,SMP7,SMP8]=deal(zeros(size(rr)));
+for i=1:numel(rr)
+Lb(i)=hnol*rr(i)*b/(1+hnol);
+h(i)=((2*b*b*rr(i))./(vf*(b*rr(i) + Lb(i)))) - (2*b);
+vf1(i)=(2*b*b*rr(i))./((2*b + h(i))*(rr(i)*b + Lb(i)));
+SMP1s(i)=regstrengthsmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(1),sigmcrit1,taumcrit1);
+SMP2s(i)=regstrengthsmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(2),sigmcrit1,taumcrit1);
+SMP3s(i)=regstrengthsmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(3),sigmcrit1,taumcrit1);
+SMP4s(i)=regstrengthsmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(4),sigmcrit1,taumcrit1);
+SMP5s(i)=regstrengthsmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(5),sigmcrit1,taumcrit1);
+SMP6s(i)=regstrengthsmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(6),sigmcrit1,taumcrit1);
+SMP1f(i)=regstrengthfmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(1),sigmcrit1,taumcrit1);
+SMP2f(i)=regstrengthfmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(2),sigmcrit1,taumcrit1);
+SMP3f(i)=regstrengthfmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(3),sigmcrit1,taumcrit1);
+SMP4f(i)=regstrengthfmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(4),sigmcrit1,taumcrit1);
+SMP5f(i)=regstrengthfmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(5),sigmcrit1,taumcrit1);
+SMP6f(i)=regstrengthfmf(Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(6),sigmcrit1,taumcrit1);
+if SMP1s(i)~=SMP1f(i)
+    rr1(i)=rr(i);
+    SMP1(i)=SMP1f(i)/SMP1s(i);
+end
+if SMP2s(i)~=SMP2f(i)
+    rr2(i)=rr(i);
+    SMP2(i)=SMP2f(i)/SMP2s(i);
+end
+if SMP3s(i)~=SMP3f(i)
+    rr3(i)=rr(i);
+    SMP3(i)=SMP3f(i)/SMP3s(i);
+end
+if SMP4s(i)~=SMP4f(i)
+    rr4(i)=rr(i);
+    SMP4(i)=SMP4f(i)/SMP4s(i);
+end
+if SMP5s(i)~=SMP5f(i)
+    rr5(i)=rr(i);
+    SMP5(i)=SMP5f(i)/SMP5s(i);
+end
+if SMP6s(i)~=SMP6f(i)
+    rr6(i)=rr(i);
+    SMP6(i)=SMP6f(i)/SMP6s(i);
+end
+end
+plot(rr1,SMP1,'linewidth',1.5)
+hold on;
+plot(rr2,SMP2,'linewidth',1.5)
+hold on;
+plot(rr3,SMP3,'linewidth',1.5)
+hold on;
+plot(rr4,SMP4,'linewidth',1.5)
+hold on;
+plot(rr5,SMP5,'linewidth',1.5)
+hold on;
+plot(rr6,SMP6,'linewidth',1.5)
+hold on;
+
+set(gca,'fontsize',15)
+
+xlabel ('\rho','fontweight','bold', 'fontsize', 15)
+ylabel ('Normalized strength ratio, reg (FMF/SMF)','fontweight','bold', 'fontsize', 15)
+set(gcf,'color','w')
+
+axis square
+ylim([0 10])
+lg1=legend('$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=10$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=25$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=50$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=100$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=150$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=200$','fontsize',9);
+lg1.FontSize=12;
+set(lg1,'Interpreter','latex')
+
+subplot(1,2,2);
+
+for i=1:numel(rr)
+Lb(i)=hnol*rr(i)*b/(1+hnol);
+h(i)=((2*b*b*rr(i))./(vf*(b*rr(i) + Lb(i)))) - (2*b);
+vf1(i)=(2*b*b*rr(i))./((2*b + h(i))*(rr(i)*b + Lb(i)));
+SMP1s(i)=swstrengthsmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(1),sigmcrit1,taumcrit1);
+SMP2s(i)=swstrengthsmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(2),sigmcrit1,taumcrit1);
+SMP3s(i)=swstrengthsmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(3),sigmcrit1,taumcrit1);
+SMP4s(i)=swstrengthsmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(4),sigmcrit1,taumcrit1);
+SMP5s(i)=swstrengthsmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(5),sigmcrit1,taumcrit1);
+SMP6s(i)=swstrengthsmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(6),sigmcrit1,taumcrit1);
+SMP1f(i)=swstrengthfmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(1),sigmcrit1,taumcrit1);
+SMP2f(i)=swstrengthfmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(2),sigmcrit1,taumcrit1);
+SMP3f(i)=swstrengthfmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(3),sigmcrit1,taumcrit1);
+SMP4f(i)=swstrengthfmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(4),sigmcrit1,taumcrit1);
+SMP5f(i)=swstrengthfmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(5),sigmcrit1,taumcrit1);
+SMP6f(i)=swstrengthfmf(n,Ep1,Em1,num1,b,Lb(i),h(i),rr(i),sigpcrit1(6),sigmcrit1,taumcrit1);
+if SMP1s(i)~=SMP1f(i)
+    rr11(i)=rr(i);
+    SMP11(i)=SMP1f(i)/SMP1s(i);
+end
+if SMP2s(i)~=SMP2f(i)
+    rr22(i)=rr(i);
+    SMP22(i)=SMP2f(i)/SMP2s(i);
+end
+if SMP3s(i)~=SMP3f(i)
+    rr33(i)=rr(i);
+    SMP33(i)=SMP3f(i)/SMP3s(i);
+end
+if SMP4s(i)~=SMP4f(i)
+    rr44(i)=rr(i);
+    SMP44(i)=SMP4f(i)/SMP4s(i);
+end
+if SMP5s(i)~=SMP5f(i)
+    rr55(i)=rr(i);
+    SMP55(i)=SMP5f(i)/SMP5s(i);
+end
+if SMP6s(i)~=SMP6f(i)
+    rr66(i)=rr(i);
+    SMP66(i)=SMP6f(i)/SMP6s(i);
+end
+end
+plot(rr11,SMP11,'linewidth',1.5)
+hold on;
+plot(rr22,SMP22,'linewidth',1.5)
+hold on;
+plot(rr33,SMP33,'linewidth',1.5)
+hold on;
+plot(rr44,SMP44,'linewidth',1.5)
+hold on;
+plot(rr55,SMP55,'linewidth',1.5)
+hold on;
+plot(rr66,SMP66,'linewidth',1.5)
+hold on;
+
+
+xlabel ('\rho','fontweight','bold', 'fontsize', 15)
+ylabel ('Normalized strength ratio, sw (FMF/SMF)','fontweight','bold', 'fontsize', 15)
+set(gcf,'color','w')
+set(gca,'fontsize',15)
+ylim([0 20])
+axis square
+lg2=legend('$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=10$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=25$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=50$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=100$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=150$','$\frac{\sigma^p_{critical}}{\tau^m_{critical}}=200$','fontsize',9);
+lg2.FontSize=12;
+set(lg2 ,'Interpreter','latex')
 toc
